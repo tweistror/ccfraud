57d1b86aeba74fd4ae3e54c2afdda308be545c60
Thomas Mahlmeister <thomas.mahlmeister@gmail.com>
2020-02-15 17:12:21 +0100

    Add sampling for normal classification

--- utils/sample_data.py
+++ utils/sample_data.py
@@ -21,3 +21,40 @@ def sample_data_for_occ(x_ben, x_fraud, dataset_string):
     y_test[n_test:] = 1
 
     return x_train, x_test, y_train, y_test
+
+
+def sample_data_for_normal_classification(x_ben, x_fraud, train_size, negative_samples, dataset_string):
+    # TODO: Handle different datasets
+
+    # Calculate dimensions
+    n_train_benign = train_size - negative_samples
+    n_train_fraud = train_size - n_train_benign
+
+    n_test = len(x_fraud)
+
+    # Shuffle
+    x_ben, x_fraud = sample_shuffle(x_ben), sample_shuffle(x_fraud)
+
+    # Testing data
+    x_test = x_ben[-n_test:].tolist() + x_fraud[-n_test:].tolist()
+    x_test = np.array(x_test)
+    y_test = np.zeros(2 * n_test)
+    y_test[n_test:] = 1
+
+    # Select correct number of benign and fraud transactions
+    x_ben = x_ben[0:n_train_benign]
+    x_fraud = x_fraud[0:n_train_fraud]
+
+    # Add labels
+    x_ben = np.append(x_ben, np.zeros((len(x_ben), 1)), axis=1)
+    x_fraud = np.append(x_fraud, np.ones((len(x_fraud), 1)), axis=1)
+
+    # Create train and test sets
+    train_data = np.concatenate((x_ben[0:n_train_benign], x_fraud[0:n_train_fraud]))
+
+    train_data = sample_shuffle(train_data)
+
+    x_train = train_data[:, :-1]
+    y_train = train_data[:, -1]
+
+    return x_train, x_test, y_train, y_test
