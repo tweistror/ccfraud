71451e9f76c3e9eb219494f7c8d786941c34a89b
Thomas Mahlmeister <thomas.mahlmeister@gmail.com>
2020-02-17 15:15:07 +0100

    Add One-Class GAN method for ccfraud-dataset

---
+++ advanced/oc_gan/oc_gan.py
@@ -0,0 +1,222 @@
+import tensorflow as tf
+import numpy as np
+
+from sklearn.metrics import classification_report, precision_recall_fscore_support
+
+from advanced.oc_gan.load_data import get_data_ccfraud
+from advanced.oc_gan.utils import xavier_init, pull_away_loss, sample_shuffle_uspv, one_hot, sample_Z, draw_trend
+
+
+def execute_oc_gan(dataset_string, usv_train, test_fraud, verbosity=0):
+    # Set parameters
+    if dataset_string == "paysim":
+        mb_size = 70
+        dim_input = 30
+    elif dataset_string == "ccfraud":
+        mb_size = 70
+        dim_input = 30
+    elif dataset_string == "ieee":
+        mb_size = 70
+        dim_input = 30
+
+    # Set dimensions for discrimator, generator and
+    D_dim = [dim_input, 100, 50, 2]
+    G_dim = [50, 100, dim_input]
+    Z_dim = G_dim[0]
+
+    # Define placeholders for labeled-data, unlabeled-data, noise-data and target-data.
+    X_oc = tf.placeholder(tf.float32, shape=[None, dim_input])
+    Z = tf.placeholder(tf.float32, shape=[None, Z_dim])
+    X_tar = tf.placeholder(tf.float32, shape=[None, dim_input])
+
+    # Declare weights and biases of discriminator.
+    D_W1 = tf.Variable(xavier_init([D_dim[0], D_dim[1]]))
+    D_b1 = tf.Variable(tf.zeros(shape=[D_dim[1]]))
+
+    D_W2 = tf.Variable(xavier_init([D_dim[1], D_dim[2]]))
+    D_b2 = tf.Variable(tf.zeros(shape=[D_dim[2]]))
+
+    D_W3 = tf.Variable(xavier_init([D_dim[2], D_dim[3]]))
+    D_b3 = tf.Variable(tf.zeros(shape=[D_dim[3]]))
+
+    theta_D = [D_W1, D_W2, D_W3, D_b1, D_b2, D_b3]
+
+    # Declare weights and biases of generator.
+    G_W1 = tf.Variable(xavier_init([G_dim[0], G_dim[1]]))
+    G_b1 = tf.Variable(tf.zeros(shape=[G_dim[1]]))
+
+    G_W2 = tf.Variable(xavier_init([G_dim[1], G_dim[2]]))
+    G_b2 = tf.Variable(tf.zeros(shape=[G_dim[2]]))
+
+    theta_G = [G_W1, G_W2, G_b1, G_b2]
+
+    # Declare weights and biases of pre-train net for density estimation.
+    T_W1 = tf.Variable(xavier_init([D_dim[0], D_dim[1]]))
+    T_b1 = tf.Variable(tf.zeros(shape=[D_dim[1]]))
+
+    T_W2 = tf.Variable(xavier_init([D_dim[1], D_dim[2]]))
+    T_b2 = tf.Variable(tf.zeros(shape=[D_dim[2]]))
+
+    T_W3 = tf.Variable(xavier_init([D_dim[2], D_dim[3]]))
+    T_b3 = tf.Variable(tf.zeros(shape=[D_dim[3]]))
+
+    theta_T = [T_W1, T_W2, T_W3, T_b1, T_b2, T_b3]
+
+    def generator(z):
+        G_h1 = tf.nn.relu(tf.matmul(z, G_W1) + G_b1)
+        G_logit = tf.nn.tanh(tf.matmul(G_h1, G_W2) + G_b2)
+        return G_logit
+
+    def discriminator(x):
+        D_h1 = tf.nn.relu(tf.matmul(x, D_W1) + D_b1)
+        D_h2 = tf.nn.relu(tf.matmul(D_h1, D_W2) + D_b2)
+        D_logit = tf.matmul(D_h2, D_W3) + D_b3
+        D_prob = tf.nn.softmax(D_logit)
+        return D_prob, D_logit, D_h2
+
+    # Pre-train net for density estimation.
+    def discriminator_tar(x):
+        T_h1 = tf.nn.relu(tf.matmul(x, T_W1) + T_b1)
+        T_h2 = tf.nn.relu(tf.matmul(T_h1, T_W2) + T_b2)
+        T_logit = tf.matmul(T_h2, T_W3) + T_b3
+        T_prob = tf.nn.softmax(T_logit)
+        return T_prob, T_logit, T_h2
+
+    D_prob_real, D_logit_real, D_h2_real = discriminator(X_oc)
+
+    G_sample = generator(Z)
+    D_prob_gen, D_logit_gen, D_h2_gen = discriminator(G_sample)
+
+    D_prob_tar, D_logit_tar, D_h2_tar = discriminator_tar(X_tar)
+    D_prob_tar_gen, D_logit_tar_gen, D_h2_tar_gen = discriminator_tar(G_sample)
+
+    # Discriminator loss
+    y_real = tf.placeholder(tf.int32, shape=[None, D_dim[3]])
+    y_gen = tf.placeholder(tf.int32, shape=[None, D_dim[3]])
+
+    D_loss_real = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=D_logit_real, labels=y_real))
+    D_loss_gen = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=D_logit_gen, labels=y_gen))
+
+    ent_real_loss = -tf.reduce_mean(
+        tf.reduce_sum(
+            tf.multiply(D_prob_real, tf.log(D_prob_real)), 1
+        )
+    )
+
+    ent_gen_loss = -tf.reduce_mean(
+        tf.reduce_sum(
+            tf.multiply(D_prob_gen, tf.log(D_prob_gen)), 1
+        )
+    )
+
+    D_loss = D_loss_real + D_loss_gen + 1.85 * ent_real_loss
+
+    # Generator loss
+    pt_loss = pull_away_loss(D_h2_tar_gen)
+
+    y_tar = tf.placeholder(tf.int32, shape=[None, D_dim[3]])
+    T_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=D_logit_tar, labels=y_tar))
+    tar_thrld = tf.divide(tf.reduce_max(D_prob_tar_gen[:, -1]) +
+                          tf.reduce_min(D_prob_tar_gen[:, -1]), 2)
+
+    indicator = tf.sign(
+        tf.subtract(D_prob_tar_gen[:, -1],
+                    tar_thrld))
+    condition = tf.greater(tf.zeros_like(indicator), indicator)
+    mask_tar = tf.where(condition, tf.zeros_like(indicator), indicator)
+    G_ent_loss = tf.reduce_mean(tf.multiply(tf.log(D_prob_tar_gen[:, -1]), mask_tar))
+
+    fm_loss = tf.reduce_mean(
+        tf.sqrt(
+            tf.reduce_sum(
+                tf.square(D_logit_real - D_logit_gen), 1
+            )
+        )
+    )
+
+    G_loss = pt_loss + G_ent_loss + fm_loss
+
+    D_solver = tf.train.GradientDescentOptimizer(learning_rate=1e-3).minimize(D_loss, var_list=theta_D)
+    G_solver = tf.train.AdamOptimizer().minimize(G_loss, var_list=theta_G)
+    T_solver = tf.train.GradientDescentOptimizer(learning_rate=1e-3).minimize(T_loss, var_list=theta_T)
+
+    # Process data
+    x_ben, x_fraud = get_data_ccfraud("ccfraud.csv")
+
+    x_ben = sample_shuffle_uspv(x_ben)
+    x_fraud = sample_shuffle_uspv(x_fraud)
+
+    x_pre = x_ben[0:usv_train]
+    y_pre = np.zeros(len(x_pre))
+    y_pre = one_hot(y_pre, 2)
+
+    x_train = x_pre
+
+    y_real_mb = one_hot(np.zeros(mb_size), 2)
+    y_fake_mb = one_hot(np.ones(mb_size), 2)
+
+    x_test = x_pre[-test_fraud:].tolist() + x_fraud[-test_fraud:].tolist()
+    x_test = np.array(x_test)
+    y_test = np.zeros(len(x_test))
+    y_test[test_fraud:] = 1
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    # Pre-training for target distribution
+    _ = sess.run(T_solver,
+                 feed_dict={
+                     X_tar: x_pre,
+                     y_tar: y_pre
+                 })
+
+    q = np.divide(len(x_train), mb_size)
+
+    d_ben_pro, d_fake_pro, fm_loss_coll = list(), list(), list()
+    f1_score = list()
+    d_val_pro = list()
+    n_round = 200
+
+    for n_epoch in range(n_round):
+        X_mb_oc = sample_shuffle_uspv(x_train)
+
+        for n_batch in range(int(q)):
+            _, D_loss_curr, ent_real_curr = sess.run([D_solver, D_loss, ent_real_loss],
+                                                     feed_dict={
+                                                         X_oc: X_mb_oc[n_batch * mb_size:(n_batch + 1) * mb_size],
+                                                         Z: sample_Z(mb_size, Z_dim),
+                                                         y_real: y_real_mb,
+                                                         y_gen: y_fake_mb
+                                                     })
+
+            _, G_loss_curr, fm_loss_curr = sess.run([G_solver, G_loss, fm_loss],
+                                                    feed_dict={Z: sample_Z(mb_size, Z_dim),
+                                                               X_oc: X_mb_oc[n_batch * mb_size:(n_batch + 1) * mb_size],
+                                                               })
+
+        D_prob_real_, D_prob_gen_ = sess.run([D_prob_real, D_prob_gen],
+                                             feed_dict={X_oc: x_train,
+                                                        Z: sample_Z(len(x_train), Z_dim)})
+
+        D_prob_fraud_ = sess.run(D_prob_real,
+                                 feed_dict={X_oc: x_fraud[-test_fraud:]})
+
+        d_ben_pro.append(np.mean(D_prob_real_[:, 0]))
+        d_fake_pro.append(np.mean(D_prob_gen_[:, 0]))
+        d_val_pro.append(np.mean(D_prob_fraud_[:, 0]))
+        fm_loss_coll.append(fm_loss_curr)
+
+        prob, _ = sess.run([D_prob_real, D_logit_real], feed_dict={X_oc: x_test})
+        y_pred = np.argmax(prob, axis=1)
+        conf_mat = classification_report(y_test, y_pred, target_names=['benign', 'vandal'], digits=4)
+        f1_score.append(float(list(filter(None, conf_mat.strip().split(" ")))[12]))
+
+    acc = np.sum(y_pred == y_test) / float(y_pred.shape[0])
+    precision, recall, fscore, support = precision_recall_fscore_support(y_test, y_pred, zero_division=0)
+
+    if verbosity ==1:
+        print(conf_mat)
+        draw_trend(d_ben_pro, d_fake_pro, d_val_pro, fm_loss_coll, f1_score)
+
+    return precision[0], recall[0], fscore[0], acc, 'OC-GAN'
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from argparse import RawTextHelpFormatter
 import numpy as np
 from datetime import datetime
 
+from advanced.oc_gan.oc_gan import execute_oc_gan
 from baselines.calculate_sv_baselines import build_supervised_baselines
 from baselines.calculate_usv_baselines import build_unsupervised_baselines
 from utils.list_operations import sample_shuffle
@@ -15,8 +16,10 @@ datasets = ["paysim", "ccfraud", "ieee"]
 parser = argparse.ArgumentParser(description='Tool for testing various machine learning methods on different datasets',
                                  formatter_class=RawTextHelpFormatter)
 parser.add_argument("--dataset", required=True, choices=datasets, help="Dataset")
-parser.add_argument("--method", choices=["sk-svm", "sk-knn", "sk-mlp", "sk-nb", "sk-rf", "sk-lr", "xgb"],
+parser.add_argument("--method", choices=["oc-gan"],
                     help="Machine learning method used for classification")
+parser.add_argument("--baselines", choices=["usv", "sv", "both"],
+                    help="Execute baselines or not")
 parser.add_argument("--mode", choices=["baseline", "solo"], help='''Execution mode: 
 `baseline` for comparison to other baseline methods
 `solo` for executing the chosen method only''')
@@ -25,22 +28,25 @@ parser.add_argument("--v", choices=['0', '1', '2'], default=0, help="Specify ver
 args = parser.parse_args()
 dataset_string = args.dataset
 verbosity = int(args.v)
+method = args.method
+baselines = args.baselines
 
 # Specify positive samples to load
 positive_samples = 10000
 
-
 # Load data
-if dataset_string == "paysim":
-    x_ben, x_fraud = get_data_paysim("paysim.csv", positive_samples=positive_samples, verbosity=verbosity)
-    x_ben = sample_shuffle(x_ben)
-elif dataset_string == "ccfraud":
-    x_ben, x_fraud = get_data_ccfraud("ccfraud.csv", positive_samples=positive_samples, verbosity=verbosity)
-    x_ben = sample_shuffle(x_ben)
-elif dataset_string == "ieee":
-    x_ben, x_fraud = get_data_ieee("ieee_transaction.csv", "ieee_identity.csv", positive_samples=positive_samples, verbosity=verbosity)
-    x_ben = sample_shuffle(x_ben)
-    x_fraud = sample_shuffle(x_fraud[0:2000])
+if baselines is not None:
+    if dataset_string == "paysim":
+        x_ben, x_fraud = get_data_paysim("paysim.csv", positive_samples=positive_samples, verbosity=verbosity)
+        x_ben = sample_shuffle(x_ben)
+    elif dataset_string == "ccfraud":
+        x_ben, x_fraud = get_data_ccfraud("ccfraud.csv", positive_samples=positive_samples, verbosity=verbosity)
+        x_ben = sample_shuffle(x_ben)
+    elif dataset_string == "ieee":
+        x_ben, x_fraud = get_data_ieee("ieee_transaction.csv", "ieee_identity.csv", positive_samples=positive_samples,
+                                       verbosity=verbosity)
+        x_ben = sample_shuffle(x_ben)
+        x_fraud = sample_shuffle(x_fraud[0:2000])
 
 # Set parameters
 usv_train, sv_train, sv_train_fraud, test_fraud = get_parameters(dataset_string)
@@ -53,45 +59,84 @@ f1_coll = list()
 acc_coll = list()
 method_list = list()
 
+method_special_list = list()
+method_usv_list = list()
+method_sv_list = list()
+
 start_time_complete = datetime.now()
 if verbosity > 0:
     print(f'Start {iteration_count} iterations')
 
 for i in range(iteration_count):
     start_time = datetime.now()
+
+    prec_list = list()
+    reca_list = list()
+    f1_list = list()
+    acc_list = list()
+
     if verbosity > 1:
-        print(f'Starting iteration #{i+1}')
+        print(f'Starting iteration #{i + 1}')
 
-    # Sample data for unsupervised learning baselines
-    x_train, x_test, y_train, y_test = sample_data_for_unsupervised_baselines(x_ben, x_fraud)
-    # Execute unsupervised learning baselines
-    prec_usv_list, reca_usv_list, f1_usv_list, acc_usv_list, method_usv_list = \
-        build_unsupervised_baselines(x_train, x_test, y_test, usv_train, test_fraud)
+    if method == 'oc-gan':
+        prec, reca, f1, acc, method_name = execute_oc_gan(dataset_string, usv_train, test_fraud)
+        prec_list = prec_list + [prec]
+        reca_list = reca_list + [reca]
+        f1_list = f1_list + [f1]
+        acc_list = acc_list + [acc]
+        if i == 0:
+            method_special_list = method_special_list + [method_name]
 
-    # Some verbosity output
-    if verbosity > 1:
-        print(f'Iteration #{i+1} unsupervised finished, supervised coming up')
+        # Some verbosity output
+        if verbosity > 1:
+            print(f'Special methods: Iteration #{i + 1} finished')
 
-    # Sample data for supervised learning baselines
-    x_train, x_test, y_train, y_test = \
-        sample_data_for_supervised_baselines(x_ben, x_fraud, sv_train, sv_train_fraud)
-    # Execute supervised learning baselines
-    prec_sv_list, reca_sv_list, f1_sv_list, acc_sv_list, method_sv_list = \
-        build_supervised_baselines(x_train, y_train, x_test, y_test, test_fraud)
+    if baselines == 'usv' or baselines == 'both':
+        # Sample data for unsupervised learning baselines
+        x_train, x_test, y_train, y_test = sample_data_for_unsupervised_baselines(x_ben, x_fraud)
+        # Execute unsupervised learning baselines
+        prec_usv_list, reca_usv_list, f1_usv_list, acc_usv_list, method_usv_list = \
+            build_unsupervised_baselines(x_train, x_test, y_test, usv_train, test_fraud)
 
-    # Add metrics for all methods to collections
-    prec_coll.append(prec_usv_list + prec_sv_list)
-    reca_coll.append(reca_usv_list + reca_sv_list)
-    f1_coll.append(f1_usv_list + f1_sv_list)
-    acc_coll.append(acc_usv_list + acc_sv_list)
+        # Add metrics to collections
+        prec_list = prec_list + prec_usv_list
+        reca_list = reca_list + reca_usv_list
+        f1_list = f1_list + f1_usv_list
+        acc_list = acc_list + acc_usv_list
+
+        # Some verbosity output
+        if verbosity > 1:
+            print(f'Unsupervised: Iteration #{i + 1} finished')
+
+    if baselines == 'sv' or baselines == 'both':
+        # Sample data for supervised learning baselines
+        x_train, x_test, y_train, y_test = \
+            sample_data_for_supervised_baselines(x_ben, x_fraud, sv_train, sv_train_fraud)
+        # Execute supervised learning baselines
+        prec_sv_list, reca_sv_list, f1_sv_list, acc_sv_list, method_sv_list = \
+            build_supervised_baselines(x_train, y_train, x_test, y_test, test_fraud)
+
+        # Add metrics to collections
+        prec_list = prec_list + prec_sv_list
+        reca_list = reca_list + reca_sv_list
+        f1_list = f1_list + f1_sv_list
+        acc_list = acc_list + acc_sv_list
+
+        # Some verbosity output
+        if verbosity > 1:
+            print(f'Supervised: Iteration #{i + 1} finished')
+
+    prec_coll.append(prec_list)
+    reca_coll.append(reca_list)
+    f1_coll.append(f1_list)
+    acc_coll.append(acc_list)
 
     if i == 0:
-        method_list = method_usv_list + method_sv_list
+        method_list = method_special_list + method_usv_list + method_sv_list
 
     if verbosity > 0:
         time_required = str(datetime.now() - start_time)
-        print(f'Iteration #{i+1} finished in {time_required}')
-
+        print(f'Iteration #{i + 1} finished in {time_required}')
 
 if verbosity > 1:
     time_required = str(datetime.now() - start_time_complete)
@@ -100,5 +145,7 @@ if verbosity > 1:
 prec_coll, reca_coll, f1_coll, acc_coll, method_list = \
     np.array(prec_coll), np.array(reca_coll), np.array(f1_coll), np.array(acc_coll), np.array(method_list)
 
-print_results(method_list, iteration_count, len(method_usv_list), prec_coll, reca_coll, f1_coll, acc_coll)
+print(prec_coll, reca_coll, f1_coll, acc_coll, method_list)
 
+print_results(method_list, iteration_count,
+              len(method_special_list), len(method_usv_list), prec_coll, reca_coll, f1_coll, acc_coll)
