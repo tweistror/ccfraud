a2afdeadb3290b1cdefdfebb08d2d5788648c3c2
Thomas Mahlmeister <thomas.mahlmeister@gmail.com>
2020-02-17 21:19:05 +0100

    Add train size to sample already

--- utils/sample_data.py
+++ utils/sample_data.py
@@ -3,22 +3,20 @@ import numpy as np
 from utils.list_operations import sample_shuffle
 
 
-def sample_data_for_unsupervised_baselines(x_ben, x_fraud):
-    n_training = len(x_ben)
+def sample_data_for_unsupervised_baselines(x_ben, x_fraud, usv_train):
     n_test = len(x_fraud)
 
     x_ben, x_fraud = sample_shuffle(x_ben), sample_shuffle(x_fraud)
 
-    x_train = x_ben[0:n_training]
+    x_train = x_ben[0:usv_train]
     x_test = x_ben[-n_test:].tolist() + x_fraud[-n_test:].tolist()
 
     x_test = np.array(x_test)
 
-    y_train = np.zeros(n_training)
     y_test = np.zeros(2 * n_test)
     y_test[n_test:] = 1
 
-    return x_train, x_test, y_train, y_test
+    return x_train, x_test, y_test
 
 
 def sample_data_for_supervised_baselines(x_ben, x_fraud, train_size, negative_samples):
