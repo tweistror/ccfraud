323fa4389fc4aac4674c826a8ead244f6d42f153
Thomas Mahlmeister <thomas.mahlmeister@gmail.com>
2020-03-04 19:44:56 +0100

    Use AUC

--- utils/run_models.py
+++ utils/run_models.py
@@ -1,35 +1,29 @@
-import numpy as np
-
-from sklearn.metrics import classification_report, precision_recall_fscore_support, confusion_matrix
+from sklearn.metrics import classification_report, precision_recall_fscore_support, confusion_matrix, roc_auc_score
 
 
 def run_usv_classification(x_test, y_test, clf, mode):
     y_test = [1 if val == 0 else -1 for val in y_test]
 
-    # TODO: Conditional statements for different modes (fraud and benign)
-    # Label for inliers/outliers have to be 1/-1 in usv
-    y_test[:n] = 1  # Benign
-    y_test[n:] = -1  # Fraud
     y_pred = clf.predict(x_test)
-    acc = np.sum(y_pred == y_test) / float(y_pred.shape[0])
+    auc_score = roc_auc_score(y_test, y_pred)
 
     # cm = confusion_matrix(y_test, y_pred)
     # class_report = classification_report(y_test, y_pred, target_names=['benign', 'fraud'], digits=4)
 
     precision, recall, fscore, support = precision_recall_fscore_support(y_test, y_pred, zero_division=0)
 
-    return precision[1], recall[1], fscore[1], acc
+    return precision[1], recall[1], fscore[1], auc_score
 
 
 def run_sv_classification(x_test, y_test, clf, mode):
     y_test = [0 if val == 0 else 1 for val in y_test]
 
     y_pred = clf.predict(x_test)
-    acc = np.sum(y_pred == y_test) / float(y_pred.shape[0])
+    auc_score = roc_auc_score(y_test, y_pred)
 
     # cm = confusion_matrix(y_test, y_pred)
     # class_report = classification_report(y_test, y_pred, target_names=['benign', 'fraud'], digits=4)
 
     precision, recall, fscore, support = precision_recall_fscore_support(y_test, y_pred, zero_division=0)
 
-    return precision[1], recall[1], fscore[1], acc
+    return precision[1], recall[1], fscore[1], auc_score
