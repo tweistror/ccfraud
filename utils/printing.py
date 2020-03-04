cf21fa99fa66969a5e7410deaa6a74250cb9beab
Thomas Mahlmeister <thomas.mahlmeister@gmail.com>
2020-03-04 19:44:14 +0100

    Use AUC

--- utils/printing.py
+++ utils/printing.py
@@ -2,7 +2,8 @@ from tabulate import tabulate
 import numpy as np
 
 
-def print_results(method_list, dataset_string, iteration_count, special_count, usv_count, prec_coll, reca_coll, f1_coll, acc_coll):
+def print_results(method_list, dataset_string, iteration_count, special_count, usv_count, prec_coll, reca_coll,
+                  f1_coll, auc_coll):
     results = list()
 
     print(f'{dataset_string}:  Average metrics over {iteration_count} iterations')
@@ -18,7 +19,7 @@ def print_results(method_list, dataset_string, iteration_count, special_count, u
         prec = f'{np.mean(prec_coll[:, index]).round(3)} \u00B1 {np.std(prec_coll[:, index]).round(3)}'
         reca = f'{np.mean(reca_coll[:, index]).round(3)} \u00B1 {np.std(reca_coll[:, index]).round(3)}'
         f1 = f'{np.mean(f1_coll[:, index]).round(3)} \u00B1 {np.std(f1_coll[:, index]).round(3)}'
-        acc = f'{np.mean(acc_coll[:, index]).round(3)} \u00B1 {np.std(acc_coll[:, index]).round(3)}'
-        results.append([method, prec, reca, f1, acc])
+        auc = f'{np.mean(auc_coll[:, index]).round(3)} \u00B1 {np.std(auc_coll[:, index]).round(3)}'
+        results.append([method, prec, reca, f1, auc])
 
-    print(tabulate(results, headers=['Method', 'Precision', 'Recall', 'F1 score', 'Accuracy']))
+    print(tabulate(results, headers=['Method', 'Precision', 'Recall', 'F1 score', 'AUC']))
