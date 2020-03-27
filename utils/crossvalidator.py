2d9cb29671faca1564d971b0e16fb6cd94ef95e1
Thomas Mahlmeister <thomas.mahlmeister@gmail.com>
2020-03-27 16:23:55 +0100

    Remove KFold (useless with imbalanced data)

--- utils/crossvalidator.py
+++ utils/crossvalidator.py
@@ -1,14 +1,13 @@
 import numpy as np
 
-from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.model_selection import StratifiedKFold
 
 from baseline_methods.evaluate_sv_baselines import build_supervised_baselines
 
 
 class Crossvalidator:
-    def __init__(self, k, cv_type, x, y):
+    def __init__(self, k, x, y):
         self.k = k
-        self.cv_type = cv_type
         self.x = x
         self.y = y
 
@@ -23,30 +22,17 @@ class Crossvalidator:
         f1_cv_list = list()
         auc_cv_list = list()
 
-        if self.cv_type == 'StratifiedKFold':
-            skf = StratifiedKFold(n_splits=self.k)
+        skf = StratifiedKFold(n_splits=self.k)
 
-            for train_index, test_index in skf.split(self.x, self.y):
-                x_train, x_test = self.x[train_index], self.x[test_index]
-                y_train, y_test = self.y[train_index], self.y[test_index]
-                precs, recas, f1s, aucs, methods = \
-                    build_supervised_baselines(x_train, y_train, x_test, y_test)
-                temp_prec_list.append(precs)
-                temp_reca_list.append(recas)
-                temp_f1_list.append(f1s)
-                temp_auc_list.append(aucs)
-
-        else:
-            kf = KFold(n_splits=self.k)
-            for train_index, test_index in kf.split(self.x):
-                x_train, x_test = self.x[train_index], self.x[test_index]
-                y_train, y_test = self.y[train_index], self.y[test_index]
-                precs, recas, f1s, aucs, methods = \
-                    build_supervised_baselines(x_train, y_train, x_test, y_test)
-                temp_prec_list.append(precs)
-                temp_reca_list.append(recas)
-                temp_f1_list.append(f1s)
-                temp_auc_list.append(aucs)
+        for train_index, test_index in skf.split(self.x, self.y):
+            x_train, x_test = self.x[train_index], self.x[test_index]
+            y_train, y_test = self.y[train_index], self.y[test_index]
+            precs, recas, f1s, aucs, methods = \
+                build_supervised_baselines(x_train, y_train, x_test, y_test)
+            temp_prec_list.append(precs)
+            temp_reca_list.append(recas)
+            temp_f1_list.append(f1s)
+            temp_auc_list.append(aucs)
 
         temp_prec_list, temp_reca_list, temp_f1_list, temp_auc_list = \
             np.array(temp_prec_list), np.array(temp_reca_list), np.array(temp_f1_list), np.array(temp_auc_list)
