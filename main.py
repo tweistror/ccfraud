feedf001be48833d62e2a57cccbd549a002aa23a
Thomas Mahlmeister <thomas.mahlmeister@gmail.com>
2020-04-29 13:11:01 +0200

    Refactor main

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-from argparse import RawTextHelpFormatter
 import numpy as np
 from datetime import datetime
-import configparser
 
 from advanced_methods.AE.autoencoder import Autoencoder
 from advanced_methods.RBM.rbm import RBM
@@ -11,53 +8,29 @@ from advanced_methods.OC_GAN.oc_gan import execute_oc_gan
 from baseline_methods.evaluate_sv_baselines import build_supervised_baselines
 from baseline_methods.evaluate_usv_baselines import build_unsupervised_baselines
 from utils.crossvalidator import Crossvalidator
-from utils.load_data import get_data_paysim, get_data_ccfraud, get_data_ieee, get_parameters, get_data_paysim_custom
+from utils.load_data import LoadData
+from utils.parameters import Parameters
+from utils.parser import Parser
 from utils.printing import print_results
-from utils.split_preprocess_data import execute_smote, split_and_preprocess_data
+from utils.smote import execute_smote
+from utils.split_preprocess_data import SplitPreprocessData
 
-datasets = ["paysim", "paysim_custom", "ccfraud", "ieee"]
+datasets = ["paysim", "paysim-custom", "ccfraud", "ieee", "nslkdd", "saperp"]
 methods = ["all", "oc-gan", "oc-gan-ae", "ae", "rbm", "vae"]
-baselines = ["both", "usv", "sv"]
+baselines = ["both", "usv", "sv", "none"]
 
-parser = argparse.ArgumentParser(description='Tool for testing various machine learning methods on different datasets',
-                                 formatter_class=RawTextHelpFormatter)
-parser.add_argument("--dataset", required=True, choices=datasets, help="Desired dataset for evaluation")
-parser.add_argument("--method", choices=methods,
-                    help="Method for evaluation (no specification will result in no evaluation of any advanced method, "
-                         "all with execute all advanced methods)")
-parser.add_argument("--baselines", choices=baselines, default="both",
-                    help="Baselines for evaluation (default is both)")
-parser.add_argument("--v", choices=['0', '1', '2'], default=0, help="Verbosity level (0 = just end results, 1 = "
-                                                                    "some timing information, "
-                                                                    "2 = more timing information)")
-parser.add_argument("--iterations", default="10", help="Desired count the specified methods are executed and evaluated")
-parser.add_argument("--cv", help="Activate crossvalidation with the desired count of train/test-splits")
-parser.add_argument("--oversampling", choices=['y', 'n'], default='n', help="Use oversampling (SMOTE) or not")
+parser = Parser(datasets, methods, baselines)
 
-args = parser.parse_args()
-dataset_string = args.dataset
-verbosity = int(args.v)
-method = args.method
-baseline = args.baselines
-iteration_count = int(args.iterations)
-use_oversampling = True if args.oversampling == 'y' else False
-cross_validation_count = 1 if args.cv is None else int(args.cv)
+dataset_string, verbosity, method, baseline, iteration_count, use_oversampling, cross_validation_count = \
+    parser.get_args()
 
 # Set parameters
-config = configparser.ConfigParser()
-config.read('config.ini')
+parameter_class = Parameters(dataset_string)
 
-usv_train, sv_train, sv_train_fraud, test_fraud, test_benign = get_parameters(dataset_string, config,
-                                                                              cross_validation_count)
+usv_train, sv_train, sv_train_fraud, test_benign, test_fraud = \
+    parameter_class.get_main_parameters(cross_validation_count)
 
-if dataset_string == "paysim":
-    x_ben, x_fraud = get_data_paysim("paysim.csv", verbosity=verbosity)
-if dataset_string == "paysim_custom":
-    x_ben, x_fraud = get_data_paysim_custom("paysim_custom.csv", verbosity=verbosity)
-elif dataset_string == "ccfraud":
-    x_ben, x_fraud = get_data_ccfraud("ccfraud.csv", verbosity=verbosity)
-elif dataset_string == "ieee":
-    x_ben, x_fraud = get_data_ieee("ieee_transaction.csv", "ieee_identity.csv", verbosity=verbosity)
+x_ben, x_fraud = LoadData(dataset_string, parameter_class.get_path()).get_data()
 
 # Initialize collections for evaluation results
 prec_coll = list()
@@ -71,6 +44,7 @@ method_usv_list = list()
 method_sv_list = list()
 
 start_time_complete = datetime.now()
+
 if verbosity > 0:
     print(f'Start {iteration_count} iterations')
 
@@ -82,10 +56,9 @@ for i in range(iteration_count):
     f1_list = list()
     acc_list = list()
 
-    x_usv_train, x_sv_train, y_sv_train, x_test, y_test = split_and_preprocess_data(dataset_string, x_ben, x_fraud,
-                                                                                    usv_train, sv_train, sv_train_fraud,
-                                                                                    test_fraud, test_benign,
-                                                                                    cross_validation_count)
+    split_preprocess_class = SplitPreprocessData(dataset_string, cross_validation_count, verbosity)
+    x_usv_train, x_sv_train, y_sv_train, x_test, y_test = \
+        split_preprocess_class.execute_split_preprocess(x_ben, x_fraud, usv_train, sv_train, sv_train_fraud, test_fraud, test_benign)
 
     # Over/undersampling
     if use_oversampling is True:
@@ -96,8 +69,9 @@ for i in range(iteration_count):
 
     if method == 'all' or method == 'oc-gan':
         prec, reca, f1, acc, method_name = execute_oc_gan(dataset_string, x_usv_train, x_test[:test_benign],
-                                                          x_test[test_benign:], test_benign, autoencoding=False,
-                                                          verbosity=verbosity)
+                                                          x_test[test_benign:], test_benign,
+                                                          parameter_class.get_oc_gan_parameters(),
+                                                          autoencoding=False, verbosity=verbosity)
         prec_list = prec_list + [prec]
         reca_list = reca_list + [reca]
         f1_list = f1_list + [f1]
@@ -107,8 +81,9 @@ for i in range(iteration_count):
 
     if method == 'all' or method == 'oc-gan-ae':
         prec, reca, f1, acc, method_name = execute_oc_gan(dataset_string, x_usv_train, x_test[:test_benign],
-                                                          x_test[test_benign:], test_benign, autoencoding=True,
-                                                          verbosity=verbosity)
+                                                          x_test[test_benign:], test_benign,
+                                                          parameter_class.get_oc_gan_parameters(),
+                                                          autoencoding=True, verbosity=verbosity)
         prec_list = prec_list + [prec]
         reca_list = reca_list + [reca]
         f1_list = f1_list + [f1]
@@ -118,7 +93,7 @@ for i in range(iteration_count):
 
     if method == 'all' or method == 'ae':
         ae_model = Autoencoder(x_usv_train, dataset_string, verbosity=verbosity)
-        ae_model.set_parameters()
+        ae_model.set_parameters(parameter_class.get_autoencoder_parameters())
         ae_model.build()
         prec, reca, f1, acc, method_name = ae_model.predict(x_test, y_test)
 
@@ -130,8 +105,8 @@ for i in range(iteration_count):
             method_special_list = method_special_list + [method_name]
 
     if method == 'all' or method == 'rbm':
-        rbm_model = RBM(x_usv_train.shape[1], 10, visible_unit_type='gauss', gibbs_sampling_steps=4,
-                        learning_rate=0.001, momentum=0.95, batch_size=512, num_epochs=10, verbosity=verbosity)
+        rbm_model = RBM(verbosity=verbosity)
+        rbm_model.set_parameters(x_usv_train.shape[1], parameter_class.get_rbm_parameters())
         prec, reca, f1, acc, method_name = rbm_model.execute(x_usv_train, x_test, y_test)
 
         prec_list = prec_list + [prec]
@@ -143,7 +118,7 @@ for i in range(iteration_count):
 
     if method == 'all' or method == 'vae':
         vae_model = VAE(x_usv_train, dataset_string, verbosity=verbosity)
-        vae_model.set_parameters()
+        vae_model.set_parameters(parameter_class.get_vae_parameters())
         vae_model.build()
         prec, reca, f1, acc, method_name = vae_model.predict(x_test, y_test)
 
